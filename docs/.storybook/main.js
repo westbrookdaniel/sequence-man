cd12bf53cbdbf205f463b1b2c038542ef85c8fd0
westy12dan <westy12dan@gmail.com>
2022-06-11 12:59:32 +0800

    Update storybook

--- docs/.storybook/main.js
+++ docs/.storybook/main.js
@@ -1,11 +1,11 @@
-const path = require("path");
+const path = require('path')
 
 module.exports = {
-  stories: ["../stories/**/*.stories.mdx", "../stories/**/*.stories.tsx"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
-  framework: "@storybook/react",
+  stories: ['../stories/**/*.stories.{tsx,mdx}'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  framework: '@storybook/react',
   core: {
-    builder: "@storybook/builder-vite",
+    builder: '@storybook/builder-vite',
   },
   async viteFinal(config, { configType }) {
     // customize the Vite config here
@@ -14,14 +14,14 @@ module.exports = {
       resolve: {
         alias: [
           {
-            find: "sequence-man",
+            find: 'sequence-man',
             replacement: path.resolve(
               __dirname,
-              "../../packages/sequence-man/"
+              '../../packages/sequence-man/'
             ),
           },
         ],
       },
-    };
+    }
   },
-};
+}
